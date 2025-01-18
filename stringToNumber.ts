86667c3f0cea81af2a02e5e5d9edd2da1b98234b
Conrad Gray <hello@conradthegray.com>
2025-01-18 01:14:32 +0000

    refactor

--- stringToNumber.ts
+++ stringToNumber.ts
@@ -1,40 +1,4 @@
-const numberWords: Record<string, number> = {
-  zero: 0,
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  ten: 10,
-  eleven: 11,
-  twelve: 12,
-  thirteen: 13,
-  fourteen: 14,
-  fifteen: 15,
-  sixteen: 16,
-  seventeen: 17,
-  eighteen: 18,
-  nineteen: 19,
-  twenty: 20,
-  thirty: 30,
-  forty: 40,
-  fifty: 50,
-  sixty: 60,
-  seventy: 70,
-  eighty: 80,
-  ninety: 90,
-};
-
-const multiplierWords: Record<string, number> = {
-  hundred: 100,
-  thousand: 1000,
-  million: 1000000,
-  billion: 1000000000,
-};
+import { WORD_TO_MULTIPLIER, WORD_TO_NUMBER } from "./constants.ts";
 
 export function stringToNumber(input: string): number {
   const words = input.split(" ");
@@ -42,10 +6,10 @@ export function stringToNumber(input: string): number {
   let currentNumber = 0;
 
   for (const word of words) {
-    if (word in numberWords) {
-      currentNumber += numberWords[word];
-    } else if (word in multiplierWords) {
-      currentNumber *= multiplierWords[word];
+    if (word in WORD_TO_NUMBER) {
+      currentNumber += WORD_TO_NUMBER[word];
+    } else if (word in WORD_TO_MULTIPLIER) {
+      currentNumber *= WORD_TO_MULTIPLIER[word];
 
       if (word !== "hundred") {
         result += currentNumber;
